c5298d4efffc6e01809545ad2ffc8e44982a3f9e
luowenjie <49091179+luowenjie1@users.noreply.github.com>
2026-03-16 20:26:38 +0800

    Improve Stata/Matlab coding style for assignment spacing

--- Data/Stata-code/Appendix/DLW-markup/prod_dlwg.m
+++ Data/Stata-code/Appendix/DLW-markup/prod_dlwg.m
@@ -1,40 +1,40 @@
 clc
 clear all
-cd ('C:\Users\yx42\Documents\Research Projects\Competition and Markup\Revision\production')
+cd('../../../production')
 
 global lnqh lagqh lnk lagk lnl lagl lnm lagm
 
-prod_est=zeros(22,10);
-for j=10:1:31
-   a=['datag_' num2str(j) '.mat'];
+prod_est = zeros(22,10);
+for j = 10:1:31
+   a = ['datag_' num2str(j) '.mat'];
     load(a)
 %OLS prediction
-xmat=[ones(size(lnqh,1),1) lnl  lnk lnm lnl.^2 lnk.^2 lnm.^2 lnl.*lnk lnl.*lnm lnk.*lnm ];
-est_OLS=(xmat'*xmat)\(xmat'*lnqh);
+xmat = [ones(size(lnqh,1),1) lnl  lnk lnm lnl.^2 lnk.^2 lnm.^2 lnl.*lnk lnl.*lnm lnk.*lnm ];
+est_OLS = (xmat'*xmat)\(xmat'*lnqh);
 %disp('OLS estimates')
 %[est_OLS]
 
 options = optimset('MaxFunEvals', 1e+10);  %'TolX',1e-10,'TolFun', 1e-6,'
-est=fminsearch(@obj_dlwg, est_OLS(2:end)',options);
+est = fminsearch(@obj_dlwg, est_OLS(2:end)',options);
 disp('input elasticity')
 [est;est_OLS(2:end)']
 
 
-al=est(1);
-ak=est(2);
-am=est(3);
-al2=est(4);
-ak2=est(5);
-am2=est(6);
-alk=est(7);
-alm=est(8);
-akm=est(9);
+al = est(1);
+ak = est(2);
+am = est(3);
+al2 = est(4);
+ak2 = est(5);
+am2 = est(6);
+alk = est(7);
+alm = est(8);
+akm = est(9);
 
 prctile(am+2*am2*lnm+akm*lnk+alm*lnl, [5 10 50 90 95])
 prctile(al+2*al2*lnl+alm*lnm+alk*lnk, [5 10 50 90 95])
 
 %save estimates
-prod_est(j-9,:)=[j est];
+prod_est(j-9,:) = [j est];
 
 end
 
